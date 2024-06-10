0b7d27a1f8545bef3f8c366b3dcfca8143de2390
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2024-06-10 10:27:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import logo from "./logo.svg";
-import * as React from "react";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
+        <p>
+          Szerkeszd át a <code>src/App.js</code> fájlt és mentsd el, hogy újratöltődjön!
+        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://legacy.reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tanulj React-ot!
         </a>
       </header>
     </div>
@@ -22,3 +23,4 @@ function App() {
 }
 
 export default App;
+
